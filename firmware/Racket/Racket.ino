af0cce3a2f7fb7fd7de8ca076cbf644fc5f0c176
Wilco Tomassen <wilco@cleverfranke.com>
2018-09-21 11:20:33 +0200

    Initial firmware (ESP01 blink)

---
+++ firmware/Racket/Racket.ino
@@ -0,0 +1,16 @@
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println("HELLO");
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  delay(500);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
+}
