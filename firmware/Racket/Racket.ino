a0f6c7c41cdb76f1798201fe233bc979eb0a8541
Wilco Tomassen <wilco@cleverfranke.com>
2018-09-27 15:14:52 +0200

    Updated firmware

--- firmware/Racket/Racket.ino
+++ firmware/Racket/Racket.ino
@@ -1,14 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>       
+#include <Adafruit_NeoPixel.h>
 
 // Networking settings
 const char* ssid = "*****";
 const char* password = "****";
 const unsigned int localPort = 8888;
 
+// LED Settings
+#define LED_PIN   2
+#define NUM_PIXELS 16
+#define OFF_COLOR 10
+
+
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 
 WiFiUDP udp;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   
@@ -18,6 +26,13 @@ void setup() {
 
   Serial.print("Connecting to: ");
   Serial.println(ssid);
+
+  // Initialize pixels to black
+  pixels.begin();
+  for (int i = 0; i < NUM_PIXELS; i++){
+    pixels.setPixelColor(i, pixels.Color(OFF_COLOR, OFF_COLOR, OFF_COLOR));
+  }
+  pixels.show();
 
   // Connecto to wifi
   WiFi.mode(WIFI_STA);
@@ -32,29 +47,66 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.flush();
+  Serial.end();
+
+  for (int i = 0; i < NUM_PIXELS; i++){
+    pixels.setPixelColor(i, pixels.Color(OFF_COLOR,160,OFF_COLOR));
+    pixels.show();
+    delay(100);
+  }
+  for (int i = NUM_PIXELS-1; i > -1; i--){
+    pixels.setPixelColor(i, pixels.Color(OFF_COLOR,OFF_COLOR,OFF_COLOR));
+    pixels.show();
+    delay(100);
+  }
 
   // Start listening for UDP packets
   udp.begin(localPort);
- 
+  
 }
 
-// the loop function runs over and over again forever
 void loop() {
 
   int packetSize = udp.parsePacket();
   if (packetSize) {
 
-    Serial.println("PACKET!");
-
-     // Read the packet into packetBufffer
+    // Read the packet into packetBufffer
     udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
 
-    // Blink led
+    // Validate packet
+    if (
+      packetBuffer[0] != 0x53       // S
+      || packetBuffer[1] != 0x51    // Q
+      || packetBuffer[4] != 0x00    // NULL
+      ) {
+       return;
+    }
+
     digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
+
+    int playerIndex = (int) packetBuffer[2];
+    int ballIndex = (int) packetBuffer[3];
+        
+    // Update pixels
+    for (int i = 0; i < NUM_PIXELS; i++) {
+      if (i == ballIndex) {
+        // Ball
+        pixels.setPixelColor(i, pixels.Color(OFF_COLOR, 160, OFF_COLOR));
+      } else if (
+        (i >= playerIndex - 1 && i <= playerIndex + 1) // Normal
+        || (playerIndex == NUM_PIXELS-1 && i == 0) // Edge A
+        || (playerIndex == 0 && i == NUM_PIXELS-1) // Edge B
+        ) {
+        // Player
+        pixels.setPixelColor(i, pixels.Color(255, OFF_COLOR,OFF_COLOR));
+      } else {
+        pixels.setPixelColor(i, pixels.Color(OFF_COLOR, OFF_COLOR, OFF_COLOR));
+      }
+    }
+    pixels.show();        
+
     digitalWrite(LED_BUILTIN, HIGH);  
   }
-  
+
 }
